8e7f85b8f86f3297ff5e566aa8304f2814513f3d
kisaragi99 <budzhalov99@gmail.com>
2022-07-15 12:30:29 +0300

    feat: show image on responce on get_image/

--- app/urls.py
+++ app/urls.py
@@ -1,28 +1,22 @@
-"""composeexample URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import os
 from django.contrib import admin
-from django.urls import include
 from django.urls import path
-from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
+from django.http import FileResponse, HttpResponse
+from random import randint
+value = randint(0, 10)
+print(value)
 
 IMG_URL = 'get_image/'
+TOMATO_IMG ="imgaes/media/Tomato.png"
+JAPAN_IMG = "imgaes/media/maxresdefault.jpg"
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PATH = os.path.join(BASE_DIR, TOMATO_IMG)
+
+def Show_image(request):
+    image_data = open(PATH, 'rb')
+    return FileResponse(image_data)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(IMG_URL, include('imgaes.urls')),
+    path(IMG_URL, Show_image),
 ]
